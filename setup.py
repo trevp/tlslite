526cf9040e05eaf0b3d185eb715f77afe9706236
Trevor <trevp@trevp.net>
2012-02-10 16:59:20 -0800

    Changing version to 0.3.9z.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup(name="tlslite",
-      version="0.3.9y",
+      version="0.3.9z",
       author="Trevor Perrin",
       author_email="tlslite@trevp.net",
       url="http://trevp.net/tlslite/",
