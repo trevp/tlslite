03b8fd949a69c141641db9af7c9b432fec78321b
Mirko Dziadzka <mirko.dziadzka@gmail.com>
2015-03-16 15:23:16 +0100

    fix session cache problem

--- tlslite/sessioncache.py
+++ tlslite/sessioncache.py
@@ -73,7 +73,7 @@ class SessionCache(object):
         try:
             #Add the new element
             self.entriesDict[bytes(sessionID)] = session
-            self.entriesList[self.lastIndex] = (sessionID, time.time())
+            self.entriesList[self.lastIndex] = (bytes(sessionID), time.time())
             self.lastIndex = (self.lastIndex+1) % len(self.entriesList)
 
             #If the cache is full, we delete the oldest element to make an
