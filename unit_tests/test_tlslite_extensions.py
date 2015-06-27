b5c690354bf9a5b95d49415797229d7169f52c23
Hubert Kario <hkario@redhat.com>
2015-06-27 19:49:27 +0200

    fix parsing server side server_name extension

--- unit_tests/test_tlslite_extensions.py
+++ unit_tests/test_tlslite_extensions.py
@@ -111,6 +111,17 @@ class TestTLSExtension(unittest.TestCase):
 
         self.assertEqual(ext.protocols, [b'http/1.1'])
 
+    def test_parse_with_SNI_server_side(self):
+        p = Parser(bytearray(
+            b'\x00\x00' +   # type of extension - SNI
+            b'\x00\x00'     # overall length - 0 bytes
+            ))
+
+        ext = TLSExtension(server=True).parse(p)
+
+        self.assertIsInstance(ext, SNIExtension)
+        self.assertEqual(ext.server_names, None)
+
     def test_parse_with_renego_info_server_side(self):
         p = Parser(bytearray(
             b'\xff\x01' +   # type of extension - renegotiation_info
@@ -414,13 +425,22 @@ class TestSNIExtension(unittest.TestCase):
             b'\x00\x00'    # length of array of names - 0 bytes
             ), server_name.write())
 
-    def test_parse(self):
+    def tes_parse_with_invalid_data(self):
+        server_name = SNIExtension()
+
+        p = Parser(bytearray(b'\x00\x01'))
+
+        with self.assertRaises(SyntaxError):
+            server_name.parse(p)
+
+    def test_parse_of_server_side_version(self):
         server_name = SNIExtension()
 
         p = Parser(bytearray(0))
 
-        with self.assertRaises(SyntaxError):
-            server_name = server_name.parse(p)
+        server_name = server_name.parse(p)
+
+        self.assertIsNone(server_name.server_names)
 
     def test_parse_null_length_array(self):
         server_name = SNIExtension()
