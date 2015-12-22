4dd8719871aac65e98de0a7661b4b05651beddd6
Hubert Kario <hkario@redhat.com>
2015-12-22 13:14:47 +0100

    clean up cryptomath imports
    by using * import we are importing a lot of extraneous
    garbage, use specific imports only

--- tlslite/utils/cryptomath.py
+++ tlslite/utils/cryptomath.py
@@ -15,7 +15,7 @@ import base64
 import binascii
 import sys
 
-from .compat import *
+from .compat import compat26Str, compatHMAC, compatLong
 
 
 # **************************************************************************
