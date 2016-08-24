756158c2949c0abc96f1daccc68ab8600cfadc9a
Hubert Kario <hkario@redhat.com>
2016-08-24 17:52:18 +0200

    python 2.7.3 compatibility
    The python on Debian 7 (Wheezy) is closer to 2.6 than to 2.7,
    for example struct.pack requires str() and will not accept
    bytearray().
    
    make the compat26Str() handle this old python correctly

--- tlslite/utils/compat.py
+++ tlslite/utils/compat.py
@@ -55,7 +55,8 @@ if sys.version_info >= (3,0):
 else:
     # Python 2.6 requires strings instead of bytearrays in a couple places,
     # so we define this function so it does the conversion if needed.
-    if sys.version_info < (2,7):
+    # same thing with very old 2.7 versions
+    if sys.version_info < (2, 7) or sys.version_info < (2, 7, 4):
         def compat26Str(x): return str(x)
     else:
         def compat26Str(x): return x
