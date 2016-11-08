987310502ec67af50f4ca164f6dd2317c553a06a
Hubert Kario <hkario@redhat.com>
2016-11-08 16:53:28 +0100

    method for finding first matching element

---
+++ tlslite/utils/lists.py
@@ -0,0 +1,24 @@
+# Authors:
+#   Hubert Kario (2016)
+#
+# See the LICENSE file for legal information regarding use of this file.
+
+"""Helper functions for handling lists"""
+
+
+def getFirstMatching(values, matches):
+    """
+    Return the first element in values that is also in matches.
+
+    Return None if values is None, empty or no element in values is also in
+    matches.
+
+    @type values: collections.abc.Iterable
+    @param values: list of items to look through, can be None
+    @type matches: collections.abc.Container
+    @param matches: list of items to check against
+    """
+    assert matches is not None
+    if not values:
+        return None
+    return next((i for i in values if i in matches), None)
