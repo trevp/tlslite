b31304188ffeed8331becd9b63247a9715862c7c
Hubert Kario <hkario@redhat.com>
2017-06-30 12:46:10 +0200

    add Python 3.6 to setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(name="tlslite-ng",
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Security :: Cryptography',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: System :: Networking'
