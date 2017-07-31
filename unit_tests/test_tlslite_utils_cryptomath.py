trevp/tlslite

commit 18160a23de0441b24fefb372531ddc5f194e0810

make the bytesToNumber much faster

tlslite/utils/cryptomath.py
@@ -15,7 +15,7 @@ import base64
 import binascii
 import sys
 
-from .compat import compat26Str, compatHMAC, compatLong
+from .compat import compat26Str, compatHMAC, compatLong, b2a_hex
 
 
 # **************************************************************************
@@ -122,9 +122,9 @@ def HKDF_expand(PRK, info, L, algorithm):
 
 def bytesToNumber(b, endian="big"):
     if endian == "big":
-        return sum(i << j for i, j in zip(b, range((len(b)-1)*8, -1, -8)))
+        return int(b2a_hex(b), 16)
     elif endian == "little":
-        return sum(i << j for i, j in zip(b, range(0, len(b)*8, 8)))
+        return int(b2a_hex(b[::-1]), 16)
     else:
         raise ValueError("Only 'big' and 'little' endian supported")
 

commit 48983fd22257e5a3e6b37e34d2d022e50b1e1b49

test for regression introduced by 18160a23de04
The DHE code expects parsing of an empty string to return zero,
as previous code did. Without it, the error handling for missing
key share is incorrect.

unit_tests/test_tlslite_utils_cryptomath.py
@@ -443,3 +443,11 @@ class TestBytesToNumber(unittest.TestCase):
     def test_with_unknown_type(self):
         with self.assertRaises(ValueError):
             bytesToNumber(bytearray(b'\xf0'), "middle")
+
+    @unittest.expectedFailure
+    def test_with_empty_string(self):
+        self.assertEqual(0, bytesToNumber(b''))
+
+    @unittest.expectedFailure
+    def test_with_empty_string_little_endian(self):
+        self.assertEqual(0, bytesToNumber(b'', "little"))