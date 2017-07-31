trevp/tlslite

commit 18160a23de0441b24fefb372531ddc5f194e0810

make the bytesToNumber much faster

tlslite/utils/cryptomath.py
@@ -15,7 +15,7 @@ import base64
 import binascii
 import sys
 
-from .compat import compat26Str, compatHMAC, compatLong
+from .compat import compat26Str, compatHMAC, compatLong, b2a_hex
 
 
 # **************************************************************************
@@ -122,9 +122,9 @@ def HKDF_expand(PRK, info, L, algorithm):
 
 def bytesToNumber(b, endian="big"):
     if endian == "big":
-        return sum(i << j for i, j in zip(b, range((len(b)-1)*8, -1, -8)))
+        return int(b2a_hex(b), 16)
     elif endian == "little":
-        return sum(i << j for i, j in zip(b, range(0, len(b)*8, 8)))
+        return int(b2a_hex(b[::-1]), 16)
     else:
         raise ValueError("Only 'big' and 'little' endian supported")
 

commit 8e0ee728b0768d0e4b8557e9c3fcaac71beb500a

fix regression introduced in 18160a23de04
correctly handle FFDHE key shares with key share being an empty string

tlslite/utils/cryptomath.py
@@ -126,6 +126,12 @@ def bytesToNumber(b, endian="big"):
 
     By default assumes big-endian encoding of the number.
     """
+    # if string is empty, consider it to be representation of zero
+    # while it may be a bit unorthodox, it is the inverse of numberToByteArray
+    # with default parameters
+    if not b:
+        return 0
+
     if endian == "big":
         return int(b2a_hex(b), 16)
     elif endian == "little":

unit_tests/test_tlslite_utils_cryptomath.py
@@ -444,10 +444,8 @@ class TestBytesToNumber(unittest.TestCase):
         with self.assertRaises(ValueError):
             bytesToNumber(bytearray(b'\xf0'), "middle")
 
-    @unittest.expectedFailure
     def test_with_empty_string(self):
         self.assertEqual(0, bytesToNumber(b''))
 
-    @unittest.expectedFailure
     def test_with_empty_string_little_endian(self):
         self.assertEqual(0, bytesToNumber(b'', "little"))