a8f4283cebf3f81d6f0b7a3979c6297e6f90bacf
Hubert Kario <hkario@redhat.com>
2017-08-08 17:54:22 +0200

    more test coverage for certificate_type

--- unit_tests/test_tlslite_messages.py
+++ unit_tests/test_tlslite_messages.py
@@ -854,6 +854,20 @@ class TestServerHello(unittest.TestCase):
         server_hello.certificate_type = CertificateType.x509
         self.assertEqual(CertificateType.x509, server_hello.certificate_type)
 
+    def test_certificate_type_set_to_raw(self):
+        server_hello = ServerHello().create(
+                (1,1),                          # server version
+                bytearray(b'\x00'*31+b'\x01'),  # random
+                bytearray(0),                   # session id
+                4,                              # cipher suite
+                1,                              # certificate type
+                None,                           # TACK ext
+                None)                           # next protos advertised
+
+        server_hello.certificate_type = 2
+        self.assertEqual(2, server_hello.certificate_type)
+
+
     def test_create_with_minimal_options(self):
         server_hello = ServerHello().create(
                 (3, 3),                                 # server version
